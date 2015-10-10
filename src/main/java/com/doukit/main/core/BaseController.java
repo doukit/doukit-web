94449f7298c03389e6cdf251541b6a6be6b1840e
guobao.jiang <loveaborn@foxmail.com>
2015-10-10 10:33:49 +0800

    springmvc4 + mybatis + log4j

---
+++ src/main/java/com/doukit/main/core/BaseController.java
@@ -0,0 +1,10 @@
+package com.doukit.main.core;
+
+/**
+ * @author Aborn Jiang
+ * @email aborn.jiang AT foxmail.com
+ * @date 10-10-2015
+ * @time 10:14 AM
+ */
+public class BaseController {
+}
---
+++ src/main/java/com/doukit/main/demo/entity/UserInfoDo.java
@@ -0,0 +1,50 @@
+package com.doukit.main.demo.entity;
+
+import java.io.Serializable;
+
+/**
+ * Author: aborn.jiang
+ * Email : aborn.jiang AT foxmail.com
+ * Date  : 4/8/15
+ * Time  : 11:52 PM
+ */
+public class UserInfoDo implements Serializable {
+
+    private static final long serialVersionUID = 2069195890284511289L;
+
+    private Integer id;
+    private String name;
+    private String address;
+
+    public UserInfoDo() {
+    }
+
+    public UserInfoDo(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
